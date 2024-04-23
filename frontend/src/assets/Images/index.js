01de50b8ae8ed1d23d34c12dce0e1248adeeb5fc
ducphongdev <dauad311@gmail.com>
2024-04-23 16:54:23 +0700

    ..

--- frontend/src/assets/Images/index.js
+++ frontend/src/assets/Images/index.js
@@ -1,8 +1,6 @@
-import logo from '@/assets/images/logo.svg';
-import noImage from '@/assets/images/user-img--erorr.png';
+import noImage from "@/assets/images/user-img--erorr.png";
 
 const images = {
-  logo,
   noImage,
 };
 export { images };
