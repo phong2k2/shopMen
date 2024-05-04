18220800e4a56a96229aee09171c2a5e58c6dab2
ducphongdev <dauad311@gmail.com>
2024-05-04 15:58:44 +0700

    Update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,23 +3,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  define: {
-    "process.env": process.env,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ command }) => {
+  const env = dotenv.config({
+    path: command === "build" ? ".env.production" : ".env.development",
+  }).parsed;
+
+  return {
+    define: {
+      "process.env": Object.keys(env).reduce((prev, key) => {
+        prev[key] = JSON.stringify(env[key]);
+        return prev;
+      }, {}),
     },
-  },
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "./src/assets/styles/index.scss" as global;`,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-  },
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "./src/assets/styles/index.scss" as global;`,
+        },
+      },
+    },
+  };
 });
 
 dotenv.config();
