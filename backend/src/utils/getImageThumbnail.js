e3b23ced2950ea9bfb3d62b974edd6bb0efa2925
ducphongdev <dauad311@gmail.com>
2024-05-07 15:05:26 +0700

    fix bug image path

--- backend/src/utils/getImageThumbnail.js
+++ backend/src/utils/getImageThumbnail.js
@@ -1,16 +1,13 @@
-const { env } = require("../configs/environment");
-
 const getImageThumbnail = (images) => {
-  const baseUrl = env.APP_URL;
   if (!images) {
     return null;
   }
 
   if (Array.isArray(images) && images[0]) {
-    return `${baseUrl}/${images[0].image}`;
+    return images[0].image;
   }
 
-  return `${baseUrl}/${images}`;
+  return images;
 };
 
 module.exports = {
